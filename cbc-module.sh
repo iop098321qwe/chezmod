46fcced5a660afa798eba11f51aa4546374fedac
Dallas Elliott <iop098321qwe@pm.me>
2025-11-26 13:51:23 -0700

    initial commit

---
+++ cbc-module.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# chezmoi aliases
+alias ch='chezmoi'
+alias chad='chezmoi add'
+alias chap='chezmoi apply'
+alias chd='chezmoi cd'
+alias chra='chezmoi re-add'
+alias chup='chezmoi update'
